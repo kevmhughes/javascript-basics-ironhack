28372ab7fb31b289dbaf4c7749dac2e6caf4b1c5
omiras <oscar.miras@gmail.com>
2024-04-17 20:25:25 +0200

    condicional negacion y diferente de

---
+++ condicional-5.js
@@ -0,0 +1,11 @@
+/**
+ * Imagina que un programa de conceder créditos (Cofidis), quiere comprobar si tienes deudas. 
+ * Si no tienes dedudas, te concedemos el crédito 
+ */
+
+let tieneDeudas = false;
+
+if (!tieneDeudas) { // si NO tienes deudas, entonces...
+    console.log("Crédito concedido.");
+}
+
---
+++ condicional-6.js
@@ -0,0 +1,12 @@
+/**
+ * A veces, simplemente quieres saber, si un valor es distinto a otro
+ * 
+ */
+
+let numCalleOscar = 6;
+let numCalleEva = 345;
+
+/** Viven Oscar y Eva en el mismo numero de calle?  != : operador 'es el valor de la izquierda DIFERENTE del valro de la derecha' ?*/
+if (numCalleEva != numCalleOscar) {
+    console.log("Ocar y Eva viven en números de calle diferente.");
+}
