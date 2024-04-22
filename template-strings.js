0c8e7f29e36b359e5f3faddcae5d1471b8fc1524
omiras <oscar.miras@gmail.com>
2024-04-22 18:39:52 +0200

    template strings

---
+++ template-strings.js
@@ -0,0 +1,10 @@
+/**
+ * Forma más cómoda de concatenar strings: template strings
+ */
+
+let edad = 5;
+let nombre = "Loki";
+
+
+// backticks ``
+console.log(`Mi gato se llama ${nombre.toUpperCase()} y tiene ${edad} años.`);
