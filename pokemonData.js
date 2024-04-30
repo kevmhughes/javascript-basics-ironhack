aeb29c585b083bf55da564a71198d04af810a004
omiras <oscar.miras@gmail.com>
2024-04-30 17:02:52 +0200

    dom

---
+++ pokemonData.js
@@ -0,0 +1,6 @@
+let pokemonData = {
+    type: "Fire",
+    name: "Charmander",
+    description: "Charmander is another one of the most beloved and recognizable Pokémon from the Pokémon franchise.",
+    imageURL: "https://img.pokemondb.net/artwork/large/charmander.jpg"
+}
