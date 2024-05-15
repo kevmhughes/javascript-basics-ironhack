f21d52817f70a2ea2fb2a5489074fd0f0257dc28
omiras <oscar.miras@gmail.com>
2024-05-15 18:18:35 +0200

    orden funcion

--- function-declarar-vs-ejecutar.js
+++ function-declarar-vs-ejecutar.js
@@ -1,4 +1,10 @@
+hola(); // primera instrucción
+
+
 function hola() {
-    console.log("HOLA");
+    console.log("HOLA"); // segunda instrucción
 }
 
+
+// Fase 1 de ejecución: JAvaScript lee todas las funciones y variables que hemos declarado.
+// Fase 2 de ejecución: JavaScript EJECUTA en orden SEQUENCIAL cada uno de las instrucciones que encuentra
---
+++ function-orden-ejecucion.js
@@ -0,0 +1,31 @@
+//Podemos definir functiones nosotros mismos.
+// Aquí por ejemplo una función que muestra por consola un mensaje
+
+function muestraPorConsola() {
+    console.log("Un mensaje!") // 2, 4
+}
+
+muestraPorConsola() // 1
+
+//Sin embargo, la gracia de las funciones es poder parametrizarlas. Es decir, que hagan algo diferente en función de la información que le suministras
+
+function muestraPorConsola_v2(mensaje) {
+    console.log(mensaje)
+}
+
+muestraPorConsola("Lo que yo ponga aquí, es lo que se va a mostrar por consola!") // 3
+
+//Las funciones pueden tener más de un parámetro. Por ejemplo, la siguiente función "suma" tiene 2. Lo que hace es sumar el valor de dichos parámetros y los muestra por consola
+
+function suma(a, b) {
+    let c = a + b; // 6,9, 12
+    console.log(c) // 7,10, 13
+}
+
+suma(1, 2) // 5
+suma(-1, -8) // 8
+suma(0, 0) // 11
+
+
+
+
